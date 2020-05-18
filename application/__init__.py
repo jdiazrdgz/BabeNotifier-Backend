b966e21fdc1f5a63eab3fed37c78a658e8cee3dd
Julio Diaz <jdiazrdgz@gmail.com>
2020-05-17 22:09:12 -0500

    Setting up firestore

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from unipath import Path
 from .config.DevelopmentConfig import DevelopmentConfig
 from .config.ProductionConfig import ProductionConfig
 from pusher import Pusher
+from google.oauth2 import service_account
+from google.cloud import firestore
 
 
 def create_app():
@@ -17,6 +20,14 @@ def create_app():
         cluster=app.config["PUSHER_APP_CLUSTER"],
         ssl=app.config["PUSHER_APP_SSL"]
     )
+    auth_file_folder = Path(__file__).ancestor(1)
+    auth_file_name = '/babenotifier-b3a726e28ca9.json'
+
+    print(str(auth_file_folder)+auth_file_name)
+    credentials = service_account.Credentials.from_service_account_file(
+        str(auth_file_folder)+auth_file_name)
+
+    fire_store = firestore.Client(credentials=credentials, project='babenotifier')
 
     with app.app_context():
         from .routes import routes
